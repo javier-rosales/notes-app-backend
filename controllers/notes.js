7d78095c44b23880b20ae56e78d1377e8ada2b11
Javier Rosales <cyjav.dev@gmail.com>
2024-11-11 14:29:04 -0600

    Add token authentication

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,17 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -26,15 +37,21 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    user: user.id
-  })
-
   try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      user: user.id
+    })
+
     const savedNote = await note.save()
     user.notes = user.notes.concat(savedNote._id)
     await user.save()
